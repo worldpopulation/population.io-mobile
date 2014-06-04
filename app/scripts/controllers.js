a5e8bbb65e50b62ffa96fb2b17b4c9e7e042bf4c
Sergey Basharov <sergey@basharov.net>
2014-06-04 14:48:38 +0400

    Added sections containers and respective controllers, directives with dummy content and styling

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -9,5 +9,54 @@
         console.log($rootScope.testChartData);
       });
 
-    });
+    })
+    .controller('StatsCtrl', function ($scope, Data, $rootScope, $location) {
+
+      Data.query(function (testChartData) {
+        $rootScope.testChartData = testChartData;
+        console.log($rootScope.testChartData);
+      });
+
+    })
+    .controller('PeopleCtrl', function ($scope, Data, $rootScope, $location) {
+
+      Data.query(function (testChartData) {
+        $rootScope.testChartData = testChartData;
+        console.log($rootScope.testChartData);
+      });
+
+    })
+    .controller('StoryCtrl', function ($scope, Data, $rootScope, $location) {
+
+      Data.query(function (testChartData) {
+        $rootScope.testChartData = testChartData;
+        console.log($rootScope.testChartData);
+      });
+
+    })
+    .controller('PositionCtrl', function ($scope, Data, $rootScope, $location) {
+
+      Data.query(function (testChartData) {
+        $rootScope.testChartData = testChartData;
+        console.log($rootScope.testChartData);
+      });
+
+    })
+    .controller('ExpectancyCtrl', function ($scope, Data, $rootScope, $location) {
+
+      Data.query(function (testChartData) {
+        $rootScope.testChartData = testChartData;
+        console.log($rootScope.testChartData);
+      });
+
+    })
+    .controller('ShareCtrl', function ($scope, Data, $rootScope, $location) {
+
+      Data.query(function (testChartData) {
+        $rootScope.testChartData = testChartData;
+        console.log($rootScope.testChartData);
+      });
+
+    })
+  ;
 }());
