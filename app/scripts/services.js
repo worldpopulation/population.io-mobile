ba67e7c92be5e488955e8ee3ea6d61518bbb48ed
Andi Weiss <andi.weiss@gmail.com>
2014-06-05 16:10:30 +0200

    finished first functional version of the timeline

--- app/scripts/services.js
+++ app/scripts/services.js
@@ -26,5 +26,39 @@
       }
     };
   })
+
+  .service('StoryService', function () {
+    return {
+      getData: function() {
+        return [{
+          year: 2051,
+          title: 'Life expactancy in Germany'
+        },{
+          year: 2044,
+          title: 'Global life expactency'
+        },{
+          year: 2014,
+          title: 'Now',
+          now: true
+        },{
+          year: 2001,
+          title: 'You turned 18!'
+        },{
+          year: 1991,
+          title: '5th billion person'
+        },{
+          year: 1986,
+          title: '4th billion person'
+        },{
+          year: 1986,
+          title: 'First man on the moon'
+        },{
+          year: 1972,
+          title: 'Born',
+          born: true
+        }];
+      }
+    };
+  })
   ;
 }());
