f5c77cd1b00e13b5418e0c374dbe7297f9b6d56e
Sergey Basharov <sergey@basharov.net>
2014-06-06 18:19:52 +0400

    Worldclock, babies stream

--- app/scripts/worldClockDirective.js
+++ app/scripts/worldClockDirective.js
@@ -6,21 +6,51 @@
       return {
         restrict: 'E',
         link: function (scope, element, attrs, ngModel) {
-          var currentValue = 12345;
-          var digits = ('' + currentValue).split('');
-          var countElement;
-          _initWorldClock()
-          setInterval(_updateWorldClock, 2500);
+          var currentValue, digits, countElement, clockElement, digit, placeholder, digitText, placeholderText, chart,
+            babiesArea, baby, babiesList, lastReborn, helloBubble, babyWidth, helloWords,
+            digitCellWidth = 20,
+            animationDuration = 500,
+            parentWidth = element[0].offsetWidth,
+            parentHeight = window.innerHeight - 300
+            ;
+          _initWorldClock();
+          _initBabiesFlood();
+          _initHelloBubble();
+          _sayHello();
+          setInterval(_updateBabiesFlood, 3000);
+          setInterval(_updateWorldClock, 3000);
+          setInterval(_sayHello, 6000);
 
           function _initWorldClock() {
-            var clockElement = d3.select(element[0])
+            currentValue = 7213007260;
+            digits = ('' + currentValue).split('');
+
+            chart = d3.select(element[0])
               .append('svg')
               .attr({
-                width: digits.length * 40,
-                height: 80,
-                style: 'margin: 60px 0 0 60px'
-              })
-              .append('g');
+                width: parentWidth,
+                height: parentHeight
+              });
+            var clip = chart.append("defs").append("svg:clipPath")
+              .attr("id", "clip")
+              .append("svg:rect")
+              .attr("id", "clip-rect")
+              .attr("x", "0")
+              .attr("y", "0")
+              .attr("width", digits.length * digitCellWidth)
+              .attr("height", 40);
+
+            clockElement = chart.append('g').attr('class', 'counter')
+              .attr({
+                'clip-path': 'url(#clip)',
+                transform: function () {
+                  var xOffset = 0;
+                  var yOffset = parentHeight - this.getBBox().height - 70;
+                  return 'translate(' + [xOffset, yOffset] + ')'
+
+                }
+              });
+
 
             countElement = clockElement
               .selectAll('.count-element')
@@ -30,87 +60,267 @@
               .attr({
                 'class': 'count-element',
                 transform: function (d, i) {
-                  return 'translate(' + [i * 40, 0] + ')';
+                  return 'translate(' + [i * digitCellWidth, 0] + ')';
                 }
               });
 
-            var digit = countElement
+            placeholder = countElement
               .append('g')
-              .attr('class', 'digit');
-
-            digit.append('rect')
               .attr({
-                fill: 'transparent',
-                width: 39,
-                height: 40
-              });
-
-            var placeholder = countElement
-              .append('g')
-              .attr('class', 'placeholder')
-              .attr({
+                class: 'placeholder',
+                'data-id': function (d, i) {return i},
                 transform: 'translate(0,40)'
               });
 
             placeholder.append('rect')
               .attr({
+//                fill: 'pink',
                 fill: 'transparent',
-                width: 39,
+                width: digitCellWidth,
                 height: 40
               });
 
-            digit.append('text')
+            placeholderText = placeholder.append('text')
               .text(function (d) {
                 return d;
               })
               .attr({
-                transform: 'translate(20,30)'
+                transform: 'translate(' + digitCellWidth / 2 + ',30)'
               });
 
-            placeholder.append('text')
+
+            digit = countElement
+              .append('g')
+              .attr(
+              {
+                class: 'digit',
+                'data-id': function (d, i) {return i}
+              });
+
+            digit.append('rect')
+              .attr({
+//                fill: 'lightblue',
+                fill: 'transparent',
+                width: digitCellWidth,
+                height: 40
+              });
+
+
+            digitText = digit.append('text')
               .text(function (d) {
                 return d;
               })
               .attr({
-                transform: 'translate(20,30)'
+                transform: 'translate(' + digitCellWidth / 2 + ',30)'
               });
 
           }
 
           function _updateWorldClock() {
+            currentValue++;
             var digits = ('' + currentValue).split('');
-            console.log(currentValue)
-            // countElement.select('.digit')
-            // console.log(countElement[0][0])
-            countElement.select('.digit')
+
+            placeholderText.text(function (d, i) { return digits[i] });
+            digit.each(function (d, i) {
+              var _digit, _placeholder;
+              if (digits[i] != d3.select(this).select('text').text()) {
+
+                _digit = d3.select(this);
+                _placeholder = d3.select('.placeholder[data-id="' + i + '"]');
+
+
+                _digit
+                  .transition()
+                  .ease('bounce')
+                  .duration(animationDuration)
+                  .delay(animationDuration)
+                  .attr({
+                    transform: 'translate(0,-40)'
+                  });
+
+                _placeholder
+                  .transition()
+                  .ease('bounce')
+                  .duration(animationDuration)
+                  .delay(animationDuration)
+                  .attr({
+                    transform: 'translate(0,0)'
+                  });
+
+                setTimeout(function () {
+                  _digit.attr('transform', 'translate(0,0)')
+                  _placeholder.attr('transform', 'translate(0,40)')
+                  digitText.text(function (d, i) { return digits[i] });
+                }, animationDuration * 2 + 100)
+
+              }
+
+            });
+
+          }
+
+          function _initBabiesFlood() {
+            babiesList = [
+              {},
+              {},
+              {},
+              {},
+              {},
+              {},
+              {},
+              {},
+              {},
+              {},
+              {},
+              {},
+              {},
+              {},
+              {},
+              {}
+            ];
+            lastReborn = babiesList.length;
+            babiesArea = chart
+              .append('g')
+              .attr({
+                class: 'babies-area',
+                transform: 'translate(0,100)'
+              })
+
+            baby = babiesArea.selectAll('.baby')
+              .data(babiesList)
+              .enter()
+              .append('g')
+              .attr({
+                class: 'baby',
+                opacity: 1
+              });
+
+            baby
+              .append("use")
+              .attr({
+                'xlink:href': function () {
+                  return ['#baby-girl', '#baby-boy'][_.random(0, 1)]
+                }
+              })
+
+            baby.each(function (d, i) {
+              babyWidth = d.babyWidth = this.getBBox().width;
+            }).attr({
+              transform: function (d, i) {
+                return 'translate(' + [i * d.babyWidth, 0] + ')'
+              }
+            })
+            babiesArea.attr('transform', function () {
+              var xOffset = parentWidth - this.getBBox().width - 50;
+              var yOffset = parentHeight - this.getBBox().height - 40;
+              return 'translate(' + [xOffset, yOffset] + ')'
+            })
+          }
+
+
+          function _updateBabiesFlood() {
+            var babies = d3.selectAll('.baby');
+
+            babies.transition()
+              .delay(100)
+              .attr(
+              {
+                'data-i': function (d, i) {return i},
+                transform: function (d, i) {
+                  return 'translate(' + [i * d.babyWidth + d.babyWidth, 0] + ')'
+                },
+                opacity: function (d, i) {
+                  if (i == babies[0].length - 1) {
+                    return 0
+                  }
+                }
+
+              }
+            )
+              .transition()
+              .attr({
+                transform: function (d, i) {
+                  if (i == babies[0].length - 1) {
+                    d3.select(this).moveToBack()
+                    return 'translate(0,0)';
+
+                  }
+                  else {
+                    return 'translate(' + [i * d.babyWidth + d.babyWidth, 0] + ')'
+                  }
+
+                }
+              })
+              .transition()
+              .attr({
+                opacity: 1
+              });
+          }
+
+          function _initHelloBubble() {
+            d3.json("scripts/hello-in-all-languages.json", function (error, json) {
+              if (error) return console.warn(error);
+              helloWords = json;
+            });
+
+
+            helloBubble = chart
+              .append('g')
+              .attr({
+                class: 'hello-bubble',
+                transform: 'translate(-200,-200)',
+                opacity: 0
+              })
+            helloBubble.append("use")
+              .attr({
+                'xlink:href': '#hello-bubble'
+
+              })
+            helloBubble.append("text")
+              .attr({
+                'fill': 'white',
+                'text-anchor': 'middle',
+                transform: 'translate(50,45)'
+
+              })
+
+          }
+
+          function _sayHello() {
+            var multiplier = _.random(0, 10);
+//            console.log(multiplier, babyWidth * multiplier)
+            helloBubble
+              .attr(
+              {
+                transform: function () {
+                  var xOffset = parentWidth - this.getBBox().width - babyWidth * multiplier - 20;
+                  var yOffset = parentHeight - this.getBBox().height - 90;
+                  return 'translate(' + [xOffset, yOffset] + ')'
+                }
+              }
+            )
+              .transition()
+              .delay(500)
+              .attr({
+                opacity: 1
+              })
               .transition()
               .delay(2000)
               .attr({
-                transform: 'translate(0,-40)'
-              });
-            countElement.select('.placeholder text')
-              .text(+currentValue++);
-            countElement.select('.placeholder')
-              .transition()
-              .ease('bounce')
-              .delay(2000)
-              .attr({
-                transform: 'translate(0,0)'
-              });
+                opacity: 0
+              })
 
-            countElement.select('.digit')
-              .transition()
-              .ease('bounce')
-              .duration(0)
-              .delay(2200)
-              .attr({
-                transform: 'translate(0,40)'
-              });
+            helloBubble.select('text').text(function () {
+              if (helloWords) {
+                var randomItem = _.random(0, helloWords.length - 1)
+                return helloWords[randomItem].greeting
+              }
+              else{
+                return 'Hello'
+              }
 
-            var a = countElement.select('.digit');
-            var b = countElement.select('.placeholder');
-            a.attr('class', 'placeholder');
-            b.attr('class', 'digit');
+
+            })
           }
         }
       };
