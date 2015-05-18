09c22ee55a6e191cb6acab66dbb5e285fc44a39b
Lukasz Caputa <lukasz@lukaszcaputa.eu>
2015-05-18 22:14:57 +0200

    Allow html tags in translation

--- app/scripts/directives.js
+++ app/scripts/directives.js
@@ -41,6 +41,12 @@
           });
         };
       }])
+      .directive('compileUnsafe', function ($compile) {
+            return function (scope, element, attr) {
+              scope.$watch(attr.compileUnsafe, function (val, oldVal) {
+                if (!val || (val === oldVal && element[0].innerHTML)) return;
+                element.html(val);
+                $compile(element)(scope);
+              })}})
   ;
 }());
-
