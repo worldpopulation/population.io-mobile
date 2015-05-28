4cdadc29c09e229f126d000bab9e20e4a949a899
Lukasz Caputa <lukasz@lukaszcaputa.eu>
2015-05-28 23:33:08 +0200

    Translation of months

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -508,7 +508,26 @@
 
       .controller('HomeCtrl', ['$scope', '$document', '$timeout', '$filter', '$location', '$rootScope', 'ProfileService', 'PopulationIOService', "Countries",
           function ($scope, $document, $timeout, $filter, $location, $rootScope, ProfileService, PopulationIOService, Countries) {
-              var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+            var getMonths = function(){
+              var month1 = $filter('translate')('MONTH1');
+              var month2 = $filter('translate')('MONTH2');
+              var month3 = $filter('translate')('MONTH3');
+              var month4 = $filter('translate')('MONTH4');
+              var month5 = $filter('translate')('MONTH5');
+              var month6 = $filter('translate')('MONTH6');
+              var month7 = $filter('translate')('MONTH7');
+              var month8 = $filter('translate')('MONTH8');
+              var month9 = $filter('translate')('MONTH9');
+              var month10 = $filter('translate')('MONTH10');
+              var month11 = $filter('translate')('MONTH11');
+              var month12 = $filter('translate')('MONTH12');
+
+              return [month1, month2, month3, month4, month5, month6, month7, month8, month9, month10, month11, month12 ];
+
+            }
+
+              var months = getMonths();
               var days = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31'];
               var years = [];
               for (var i = 1920; i < new Date().getFullYear(); i++) { years.push(i.toString()) }
@@ -525,6 +544,13 @@
               $scope.setCountry = function ($item, $model, $label) {
                   $scope.goForm.country.$setValidity('validateCountry', true);
               };
+
+              $scope.$on('languageChange', function () {
+                months = getMonths();
+              });
+
+
+
 
 
               $scope.$watch('goForm.$invalid', function (invalid) {
